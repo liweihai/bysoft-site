c122e5f8d2b3a277ea6dcda9d66d5e9dc31e98ea
liweihai <weihailee@sohu.com>
2025-06-24 01:16:11 +0800

    change login ui

--- app/(admin)/dashboard/layout.tsx
+++ app/(admin)/dashboard/layout.tsx
@@ -2,25 +2,33 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 
 import { signOut } from '@/auth';
 import SideNav from '@/components/dashboard/SideNav';
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-        <form
-          action={async () => {
-            'use server';
-            await signOut({ redirectTo: '/' });
-          }}
-        >
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
-            <PowerIcon className="w-6" />
-            <div className="hidden md:block">退出</div>
-          </button>
-        </form>
+    <div>
+      <Header />
+
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+          <SideNav />
+          <form
+            action={async () => {
+              'use server';
+              await signOut({ redirectTo: '/' });
+            }}
+          >
+            <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+              <PowerIcon className="w-6" />
+              <div className="hidden md:block">退出</div>
+            </button>
+          </form>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      
+      <Footer />
     </div>
   );
 }
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,5 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: '首页' },
+  { href: '/dashboard', title: '后台' },
 ]
 
 export default headerNavLinks
