5366298fce2b3792038680b7970d8e6d02463107
liweihai <weihailee@sohu.com>
2025-07-02 12:27:37 +0800

    refactor

--- lib/data.ts
+++ lib/data.ts
@@ -3,20 +3,7 @@ import { CredentialsSignin } from 'next-auth';
 import { Account, ServerError } from "./definitions";
 
 export async function login(username: String, password: String): Promise<Account> {
-    const { env, cf, ctx } = await getCloudflareContext({async: true});
-
-    const utf8Array = new TextEncoder().encode(JSON.stringify({ username: username, password: password }))
-
-    const url = env.API_HOST + "/test/account/login?_code=" + env.API_CODE ;
-    const options = {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
-        },
-        body: utf8Array
-    };
-    const response = await fetch(url, options);
+    const response = await callApi("/account/login", { username: username, password: password });
     const account : Account | ServerError = await response.json();
 
     if ("token" in account) {
@@ -27,21 +14,7 @@ export async function login(username: String, password: String): Promise<Account
 }
 
 export async function findModels<T>(modelName: String, conditions = {}, filter = {}): Promise<T[]> {
-    const { env, cf, ctx } = await getCloudflareContext({async: true});
-
-    const utf8Array = new TextEncoder().encode(JSON.stringify({conditions: conditions || {}, filter: filter || {}}))
-
-    const options = {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
-        },
-        body: utf8Array
-    };
-
-    const url = env.API_HOST + "/test/select/" + modelName + "?_code=" + env.API_CODE
-    const response = await fetch(url, options);
+    const response = await callApi("/select/" + modelName, {conditions: conditions || {}, filter: filter || {}});
 
     try {
         const blogs: T[] =  await response.json();
@@ -54,21 +27,7 @@ export async function findModels<T>(modelName: String, conditions = {}, filter =
 }
 
 export async function countModels(modelName: String, conditions: {}): Promise<number> {
-    const { env, cf, ctx } = await getCloudflareContext({async: true});
-
-    const utf8Array = new TextEncoder().encode(JSON.stringify({conditions: conditions || {}}))
-
-    const options = {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
-        },
-        body: utf8Array
-    };
-
-    const url = env.API_HOST + "/test/count/" + modelName + "?_code=" + env.API_CODE;
-    const response = await fetch(url, options);
+    const response = await callApi("/count/" + modelName, {conditions: conditions || {}});
 
     try {
         const total =  await response.text();
@@ -81,12 +40,7 @@ export async function countModels(modelName: String, conditions: {}): Promise<nu
 }
 
 export async function getModel<T>(modelName: String, id:String): Promise<T> {
-    const { env, cf, ctx } = await getCloudflareContext({async: true});
-
-    const url = env.API_HOST + "/test/select/" + modelName + "?_code=" + env.API_CODE + "&id=" + id;
-    const response = await fetch(url, {headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
-    }});
+    const response = await callApi("/select/" + modelName, {conditions: {id: id}, filter: {limit: 1, offset: 0}});
 
     try {
         const models: T[] =  await response.json();
@@ -100,21 +54,7 @@ export async function getModel<T>(modelName: String, id:String): Promise<T> {
 }
 
 export async function createModel<T>(modelName: String, model:T): Promise<T> {
-    const { env, cf, ctx } = await getCloudflareContext({async: true});
-
-    const utf8Array = new TextEncoder().encode(JSON.stringify(model))
-
-    const url = env.API_HOST + "/test/create/" + modelName + "?_code=" + env.API_CODE;
-    const options = {
-        method: 'POST',
-        headers: { 
-            'Content-Type': 'application/json',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'
-        },
-        body: utf8Array
-    };
-
-    const response = await fetch(url, options);
+    const response = await callApi("/create/" + modelName, model);
 
     try {
         const blogs: T[] =  await response.json();
@@ -126,11 +66,17 @@ export async function createModel<T>(modelName: String, model:T): Promise<T> {
 }
 
 export async function updateModel<T>(modelName: String, id: String, model:T): Promise<T> {
+    const response = await callApi("/update/" + modelName + "/" + id, model);
+
+    const newModel: T =  await response.json();
+
+    return newModel
+}
+
+export async function callApi(path: string, data: {}): Promise<Response> {
     const { env, cf, ctx } = await getCloudflareContext({async: true});
 
-    const utf8Array = new TextEncoder().encode(JSON.stringify(model))
-
-    const url = env.API_HOST + "/test/update/" + modelName + "/" + id + "?_code=" + env.API_CODE;
+    const utf8Array = new TextEncoder().encode(JSON.stringify(data))
     const options = {
         method: 'POST',
         headers: { 
@@ -139,10 +85,8 @@ export async function updateModel<T>(modelName: String, id: String, model:T): Pr
         },
         body: utf8Array
     };
-
-    const response = await fetch(url, options);
-
-    const newModel: T =  await response.json();
-
-    return newModel
+    
+    const url = env.API_HOST + "/test" + path + "?_code=" + env.API_CODE ;
+    
+    return await fetch(url, options);
 }
