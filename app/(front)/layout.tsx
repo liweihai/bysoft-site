23a9138f44d418bb726360538c9bb77a5d41c398
liweihai <weihailee@sohu.com>
2025-07-13 00:40:48 +0800

    add google tag manager

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -4,7 +4,7 @@ import { Space_Grotesk } from 'next/font/google'
 import { ThemeProvider } from 'next-themes'
 import { Metadata } from 'next'
 import { useEffect } from 'react';
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
@@ -60,27 +60,27 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     const basePath = process.env.BASE_PATH || ''
 
     return (
-      <html
-        lang={siteMetadata.language}
-        className={`${space_grotesk.variable} scroll-smooth`}
-        suppressHydrationWarning
-      >
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href={`${basePath}/logo.png`}
-        />
-        <meta name="msapplication-TileColor" content="#000000" />
-        <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
-        <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
-        <GoogleAnalytics gaId="'G-53T9PKVYR1" />
-        
-        <body className="bg-gray-200 font-sans font-thin px-6 bg-fixed bg-cover bg-no-repeat" style={{backgroundImage: "url('https://images.unsplash.com/photo-1523742238290-adf3b54101bf?w=1800')"}}>
-            <Header />
-            <main className="mx-auto max-w-6xl bg-white py-20 px-12 shadow-xl mb-24">{children}</main>
-            <Footer />
-        </body>
-      </html>
+        <html
+          lang={siteMetadata.language}
+          className={`${space_grotesk.variable} scroll-smooth`}
+          suppressHydrationWarning
+        >
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href={`${basePath}/logo.png`}
+            />
+            <meta name="msapplication-TileColor" content="#000000" />
+            <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+            <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+            <Script id="gtm">{"(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K5XGRDN8');"}</Script>
+            <body className="bg-gray-200 font-sans font-thin px-6 bg-fixed bg-cover bg-no-repeat" style={{backgroundImage: "url('https://images.unsplash.com/photo-1523742238290-adf3b54101bf?w=1800')"}}>
+                <Header />
+                <main className="mx-auto max-w-6xl bg-white py-20 px-12 shadow-xl mb-24">{children}</main>
+                <Footer />
+            </body>
+
+        </html>
     )
 }
