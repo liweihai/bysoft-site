a5cdef79994f00b4c6a08f379f2245f239ea4f64
liweihai <weihailee@sohu.com>
2025-07-21 23:21:54 +0800

    use new login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,70 +1,78 @@
 'use client';
  
 import {
-  AtSymbolIcon,
-  KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { useActionState } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 import { authenticate } from '@/lib/actions';
- import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 export default function LoginForm() {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
-  const [errorMessage, formAction, isPending] = useActionState(
-    authenticate,
-    undefined,
-  );
- 
-  return (
-    <form action={formAction} className="space-y-3">
-      <div className="flex-1 rounded-lg px-6 pb-4 pt-8">
-        <div className="py-6 flex flex-col justify-center sm:py-12">
-          <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-            <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
-            </div>
-            <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-              <div className="max-w-md mx-auto">
-                <div>
-                  <h1 className="text-2xl font-semibold">欢迎回来</h1>
-                </div>
-                <div className="divide-y divide-gray-200">
-                  <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                    <div className="relative">
-                      <input autoComplete="off" id="username" name="username" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="用户名" />
-                      <label htmlFor="username" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">用户名</label>
-                    </div>
-                    <div className="relative">
-                      <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="密码" />
-                      <label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">密码</label>
-                    </div>
-                    <input type="hidden" name="redirectTo" value={callbackUrl} />
-                    <div className="relative mt-5">
-                      <Button>登 录</Button>
-                    </div>
+    const searchParams = useSearchParams();
+    const callbackUrl  = searchParams.get('callbackUrl') || '/dashboard';
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    );
+  
+    return (
+        <div className="flex flex-col gap-6">
+          <Card className="relative py-6 sm:max-w-xl sm:mx-auto w-[24rem]">
+              <CardHeader>
+                  <CardTitle>欢迎回来</CardTitle>
+                  <CardDescription>
+                    请输入您的电子邮箱登录
+                  </CardDescription>
+              </CardHeader>
+              <CardContent>
+                  <form action={formAction}>
+                      <input type="hidden" name="redirectTo" value={callbackUrl} />
+                      <div className="flex flex-col gap-6">
+                          <div className="grid gap-3">
+                              <Label htmlFor="email">电子邮箱</Label>
+                              <Input
+                                id="email"
+                                name="username"
+                                type="text"
+                                placeholder="m@example.com"
+                                required
+                              />
+                          </div>
+                          <div className="grid gap-3">
+                              <div className="flex items-center">
+                                <Label htmlFor="password">密码</Label>
+                              </div>
+                              <Input id="password" name="password" type="password" required />
+                          </div>
+                          <div className="flex flex-col gap-3">
+                              <Button type="submit" className="w-full">
+                                登 录
+                              </Button>
+                          </div>
+                      </div>
+                  </form>
+                  <div className="flex h-8 items-end space-x-1" aria-live="polite" aria-atomic="true">
+                      {errorMessage && (
+                        <>
+                          <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                          <p className="text-sm text-red-500">{errorMessage}</p>
+                        </>
+                      )}
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
+              </CardContent>
+
+          </Card>
         </div>
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {errorMessage && (
-            <>
-              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-              <p className="text-sm text-red-500">{errorMessage}</p>
-            </>
-          )}
-        </div>
-      </div>
-    </form>
-  );
+    );
 }
